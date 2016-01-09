1ff5fb9be17a62a163a10a2d0b1740e53465ad6a
Theo Beffart <tbeffart@gmail.com>
2016-01-09 15:39:08 +0100

    remove comment deletion from buildscript

--- scripts/buildtex.sh
+++ scripts/buildtex.sh
@@ -1,5 +1,5 @@
-python2 stripPumlComments.py ../diagrams/plantuml/architecture.puml > temp
-./plant2tex temp > includes
+#python2 stripPumlComments.py ../diagrams/plantuml/architecture.puml > temp
+./plant2tex ../diagrams/plantuml/architecture.puml > includes
 mv *.tex ../latex 
 mv includes ../latex/latexinput.tex
 rm temp
